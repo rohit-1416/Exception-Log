4d05126871d9e553be9f3e86485ee6d174cafc41
rohit-1416 <rohitkulkarni4321@gmail.com>
2021-03-07 10:36:44 +0530

    responsive design

--- student-details-form.php
+++ student-details-form.php
@@ -13,51 +13,74 @@
 </head>
 <body>
    
-    <div class="cointainer mt-5">
+    <div class="cointainer m-5 p-3 border border-danger">
+
     <!--*********************START HARE********************-->
  <form action="dbopration.php" method="POST" class="" >
  
-     <div class="form-roup"> 
-        <label for="name">Name:</label><br>
-        <input type="text" id="name" name="name" placeholder="Enter Your Name" class="form-control"><br>
-        <label for="email">Email Address:</label><br>
-        <input type="email" name="email" placeholder="Enter Your Email"class="form-control"><br>
-        <label for="number">Mobile Number:</label><br>
-        <input type="number" name="number" placeholder="Entaer your Mobile Number" class="form-control"><br>
-        <label for="date">Select Date of Birth</label><br>
-        <input type="date" name="date" id="date" class="form-control"><br>
+     <div class="form-roup row">
+         <div class="col-sm-6 col-lg-3">
+            <label for="name">Name:</label><br>
+            <input type="text" id="name" name="name" placeholder="Enter Your Name" class="form-control"><br>
         </div>
-        <div class="">
-        <label for="listbox">Year Of Study</label><br>
-        <select Name="listbox" class="form-control">  <br>
-         <option> 1st Year </option>  
-         <option> 2nd Year  </option>  
-         <option> 3rd Year  </option>  
-         <option> Last Year </option>  
-        </select> <br> 
-   </div>
-
-   <div class="form-check">
-
-        <label for="radio">Select Gender:</label><br>
-             <input type="radio" name="gender" id="male" name="gender" value="male" class="form-check-input" chacked >
-            <label for="male">Male</label><br>
-            <input type="radio" id="female" name="gender" value="female" class="form-check-input">
-            <label for="female">Female</label><br>
-            <input type="radio" id="other" name="gender" value="other" class="form-check-input">
-            <label for="other">Other</label><br>
-        
-        
-        <label for="checkbox">Select Your Subjects</label><br>
-        <input type='checkbox' name='check_list[]' value='java' checked class="form-check-input">Java<br>
-        <input type='checkbox' name='check_list[]' value='php'class="form-check-input">PHP<br>
-        <input type='checkbox' name='check_list[]' value='python'class="form-check-input">Python<br>
-
-        <textarea rows="4" cols="30" name="address" id="address" placeholder="Enter Your Address" class="form-control">  </textarea>  
+        <div class="col-sm-6 col-lg-3">
+            <label for="email">Email Address:</label><br>
+            <input type="email" name="email" placeholder="Enter Your Email"class="form-control"><br>
         </div>
-        <input type="submit" name="submit" id="submit" class="btn btn-success">
-        <input type="reset" name="reset" id="reset" class="btn btn-danger" style="">   
-        <a href ="retrive.php" class="btn btn-info" >View all data</a>
+        
+        <div class="col-sm-6 col-lg-3">
+            <label for="number">Mobile Number:</label><br>
+            <input type="number" name="number" placeholder="Entaer your Mobile Number" class="form-control"><br>
+        </div>
+        
+        <div class="col-sm-6 col-lg-3">
+            <label for="date">Select Date of Birth</label><br>
+             <input type="date" name="date" id="date" class="form-control"><br>
+        </div>
+      </div>
+
+    <div class=" row">
+        <div class="col-sm-6 col-lg-3">
+                <label for="listbox">Year Of Study</label><br>
+                <select Name="listbox" class="form-control">  <br>
+                    <option> 1st Year </option>  
+                    <option> 2nd Year  </option>  
+                    <option> 3rd Year  </option>  
+                    <option> Last Year </option>  
+                </select> <br> 
+            </div>
+      
+
+         <div class="form-check col-sm-6 col-lg-3 ml-3">
+
+                <label for="radio">Select Gender:</label><br>
+                <input type="radio" name="gender" id="male" name="gender" value="male" class="form-check-input" checked >
+                <label for="male">Male</label><br>
+                <input type="radio" id="female" name="gender" value="female" class="form-check-input">
+                <label for="female">Female</label><br>
+                <input type="radio" id="other" name="gender" value="other" class="form-check-input">
+                <label for="other">Other</label><br>
+            </div>
+        
+        <div class=" form-check col-sm-6 col-lg-3 ml-3">
+            <label for="checkbox">Select Your Subjects</label><br>
+            <input type='checkbox' name='check_list[]' value='java' checked class="form-check-input">Java<br>
+            <input type='checkbox' name='check_list[]' value='php'class="form-check-input">PHP<br>
+            <input type='checkbox' name='check_list[]' value='python'class="form-check-input">Python<br>
+        </div>
+    </div>
+        
+        <label for="address">Address: </label><br> 
+        <div class="input-group">
+            <textarea name="address" id="address" rows="5"  class="form-control" placeholder="Enter Your Address"></textarea>  
+        </div>
+        
+        <div>
+            <input type="submit" name="submit" id="submit" class="btn btn-success btn-lg">
+            <input type="reset" name="reset" id="reset" class="btn btn-danger btn-lg" style="">   
+            <a href ="retrive.php" class="btn btn-info btn-lg" >View all data</a>
+        </div>
+    
     </form>
 
 
